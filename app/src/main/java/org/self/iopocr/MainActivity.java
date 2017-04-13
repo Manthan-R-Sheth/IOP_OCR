6ebc760c3e3a2b754a9d55a775d6e655832b9749
manthan-r-sheth <manthanrsheth96@gmail.com>
2017-04-14 00:50:05 +0530

    Set up alarm manager

--- app/src/main/java/org/self/iopocr/MainActivity.java
+++ app/src/main/java/org/self/iopocr/MainActivity.java
@@ -1,10 +1,17 @@
 package org.self.iopocr;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -46,8 +53,29 @@ public class MainActivity extends AppCompatActivity {
         receivePhoto();
         //update DB
         updateDB();
+        //notification
+        scheduleNotification(getNotification("Your form has been recorded successfully"),1000);
     }
 
+    private void scheduleNotification(Notification notification, int delay) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private Notification getNotification(String content) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Scheduled Notification");
+        builder.setContentText(content);
+        return builder.build();
+    }
     private void updateDB() {
         URL url;
         HttpURLConnection conn = null;
